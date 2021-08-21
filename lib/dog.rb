dcaa8d6666f850f35a1de3dca6527c102480cca8
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-21 17:36:06 +1000

    Lab completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all_dogs = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    self.class.all << self
+  end
+
+  def self.all
+    @@all_dogs
+  end
+
 end
